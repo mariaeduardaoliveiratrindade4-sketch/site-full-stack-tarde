3c2127ae8decbff32e0c71c4fceec9655dfdeaab
lavine alves <lavinealvx@gmail.com>
2026-06-26 16:34:41 -0300

    atualiza o arquivo scripts/script.js

---
+++ scripts/script.js
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+    <meta charset= "UTF-8">
+    <meta name="viewport" content="width=device=width, initial=scale=1.0">
+    <link rel="stylesheet" href="styles/style.css>
+      <title>site full stack tarde</title">
+
+        styles/style.css
+        </head>
+        </body>
+            <header>
+                <h1>projeto site full stack tarde</h1>
+                </p>exemplo de projetoutilizando Git, GitHub e trello.</p>
+            </header>
+
+            <main>
+                </section>
+                <h2>Sobre o projeto</h2>
+                <p>
+                    Este projeto foi criado para praticar versionamento com Git, 
+                    hospedagem no GitHub  e gerenciamento de tarefas com trello.
+                </p>
+            </section> 
+            </section>
+            <h2>Equipe/h2>
+                <ul>
+                    <li>maria eduarda</li>
+                    <li>lavine alves</li>
+
+                <>
+                    <main>
+                        <button onClick="mostrar mensagem()">
+                            clique aqui
+                        </button>
+                    <p id="mensagem"></p>
+                    </main>
+                    <footer
+                    <p>&copy; 2026 - site full stack tarde</p>
+                    </footer>
+                    <script src="script/script.js"><</script>
+                    </body>
+                    </html>                    
